1e0bacd806f54daba66297fabae42af1043306ea
ALI SHAIKH 1973 <139847421+alishaikh1973@users.noreply.github.com>
2023-09-27 02:04:17 +0530

    Add files via upload

---
+++ SingleDelegates.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSPROJECT_TQ.Delegates
+{
+    internal class SingleDelegates
+    {
+
+        public delegate int MyDelegate(int x, int y);
+
+        public delegate double Delegate1(double x, double y);
+        public class Calculation
+        {
+            public int Addition(int a, int b)
+            {
+                return a + b;
+            }
+
+            public double Division(double a, double b)
+            {
+                return a / b;
+            }
+        }
+    }
+
+
+}
+
---
+++ User2.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOPSPROJECT_TQ.Delegates
+{
+    internal class User2
+
+    {
+        public delegate string MyDelegate1(string Name);
+        
+
+        
+            public static string UpperCase(string Name)
+
+            {
+                return Name.ToUpper();
+
+            }
+            
+         
+           
+            
+        
+
+      
+    }
+}
